009aefb3706a1695044524c81816af2a2b99429b
Bernardo Ronquillo <bernardo.ronquillo@universaldx.com>
2020-04-27 12:36:45 +0200

    Card.svelte: Add style tag to define 'h5.card-title' to have green color

--- src/Card.svelte
+++ src/Card.svelte
@@ -2,6 +2,10 @@
 export let data;
 </script>
 
+<style>
+    h5.card-title { color:green; }
+</style>
+
 <div class="card mt-5">
     <div class="card-body">
         <h5 class="card-title">{data}</h5>
